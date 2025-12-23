c93c36e5dc29096f9f71929441bfc4671bbf104e
Albreto Suryanata Timbung <albretostimbung@gmail.com>
2025-12-23 18:35:06 +0700

    feat: add landing page

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Auth\GoogleAuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/auth/google', [GoogleAuthController::class, 'redirect'])
